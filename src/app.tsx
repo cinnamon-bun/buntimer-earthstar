06102d1ec75ae21f1cc91234221b614eb1131f52
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-03-05 21:30:37 -0800

    enable debug logging to help find storage closure bugs

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,7 @@ import {
     LocalStorageSettingsWriter,
     useLocalStorageEarthstarSettings,
 } from 'react-earthstar';
+import { setLogLevels } from 'earthstar';
 
 import {
     TimerApp
@@ -15,7 +16,15 @@ import 'react-earthstar/styles/layout.css';
 import 'react-earthstar/styles/junior.css';
 import './css/earthbar-override.css';
 
+
 function App() {
+    // 0: error, 1: warn, 2: log, 3: debug
+    setLogLevels({
+        sync: 2,
+        syncer2: 2,
+        storage: 2,
+        _other: 2
+    });
     const initValues = useLocalStorageEarthstarSettings('buntimer');
     return (
         <div className="root">
